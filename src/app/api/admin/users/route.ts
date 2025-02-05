c1d36929b5e71f104cbc65404a18e545c023054d
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-02-05 12:06:08 +0530

    Feat: added base for blocking the user

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -4,6 +4,7 @@ import Logger from "@/lib/LoggerHelper";
 import { GetUserGivenAccessToken } from "@/lib/tokenHelper";
 import { cookies } from "next/headers";
 import UserServiceInstance from "@/service/UserService";
+import { isObjectIdOrHexString } from "mongoose";
 export async function GET(request:NextRequest){
     try {
         Logger._logger.info(request.url)
@@ -15,6 +16,26 @@ export async function GET(request:NextRequest){
         const page=searchParams.get('page')??"0"
         const users=await UserServiceInstance.getAllUsersAdmin(parseInt(page))
         return NextResponse.json({message:HttpStatusMessage[HttpStatus.OK],users:users},{status:HttpStatus.OK})
+    } catch (error) {
+        const controllerError=error as Error
+        Logger._logger.error(controllerError.message)
+        return NextResponse.json({message:HttpStatusMessage[HttpStatus.INTERNAL_SERVER_ERROR]},{status:HttpStatus.INTERNAL_SERVER_ERROR})
+    }
+}
+
+export async function PATCH(request:NextRequest){
+    try {
+        const user=await GetUserGivenAccessToken(cookies())
+        if(!user || user.role!=="admin"){
+            return NextResponse.json({message:HttpStatusMessage[HttpStatus.UNAUTHORIZED]},{status:HttpStatus.UNAUTHORIZED}) 
+        }
+        const {userId}=await request.json()
+
+        if(!userId || !isObjectIdOrHexString(userId)){
+            return NextResponse.json({message:HttpStatusMessage[HttpStatus.BAD_REQUEST]},{status:HttpStatus.BAD_REQUEST})
+        }
+        return NextResponse.json({message:HttpStatusMessage[HttpStatus.OK]},{status:HttpStatus.OK})
+
     } catch (error) {
         const controllerError=error as Error
         Logger._logger.error(controllerError.message)
