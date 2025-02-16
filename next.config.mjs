08e325717036f0ac8545e06696996c4f5115d899
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-02-17 00:23:42 +0530

    Feat: final try on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images:{
         domains:["pixcap.com","res.cloudinary.com"]
     },
+    serverRuntimeConfig: {
+        maxRequestTimeout: 10 * 60 * 1000, // 10 minutes
+      },
     
     
 };
